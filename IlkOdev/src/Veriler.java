770c2a76bf59ab7b2da8dfb673caa422f672f0b4
Sedat Doğan <dogannnsedat@gmail.com>
2022-09-15 16:30:58 +0300

    Temel veriler yazıldı

---
+++ IlkOdev/src/Veriler.java
@@ -0,0 +1,14 @@
+
+public class Veriler {
+
+	public static void main(String[] args) {
+		int sayi=10;
+		float sayiF=10.0f;
+		double sayiD=12.5;
+		String isim="Sedat";
+		char karakter='S';
+		boolean mantiksal=true;
+		System.out.println(sayiF);
+	}
+
+}
