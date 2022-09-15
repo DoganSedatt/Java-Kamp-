d3af2a4bab706050583a28a60573e23de33e806f
Sedat Doğan <dogannnsedat@gmail.com>
2022-09-15 16:56:52 +0300

    ReCap Demo-En büyük sayıyı bul

---
+++ IlkOdev/src/enBuyukSayi.java
@@ -0,0 +1,20 @@
+
+public class enBuyukSayi {
+
+	public static void main(String[] args) {
+		int sayi1=25;
+		int sayi2=26;
+		int sayi3=20;
+		int enBuyuk=sayi1;
+		
+		if(enBuyuk<sayi2) {
+			enBuyuk=sayi2;
+		}
+		
+		if(enBuyuk<sayi3) {
+			enBuyuk=sayi3;
+		}
+	System.out.println("En büyük sayı="+ enBuyuk);	
+	}
+
+}
