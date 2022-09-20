98ed8ab7cb23b408c43e8f16debd23c56e03d118
Sedat Doğan <dogannnsedat@gmail.com>
2022-09-20 19:52:56 +0300

    Dizi örnek-1

---
+++ IlkOdev/src/Diziler.java
@@ -0,0 +1,13 @@
+
+public class Diziler {
+
+	public static void main(String[] args) {
+		String[] ogr=new String[3];
+		ogr[0]="Sedat";
+		ogr[1]="Doğan";
+		ogr[2]="Zeynep";
+		for(int i=0;i<ogr.length;i++) {
+			System.out.println(ogr[i]);
+		}
+	}
+}
